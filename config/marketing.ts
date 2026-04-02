e4fc3359a0f38f93330453e84b9a35492ae0f3f3
o614 <49578959+o614@users.noreply.github.com>
2026-04-02 11:36:07 +0800

    Update marketing.ts

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,20 +3,32 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Features",
-      href: "/#features",
+      title: "应用查价",
+      href: "/apps",
     },
     {
-      title: "Pricing",
-      href: "/pricing",
+      title: "内购透视",
+      href: "/iap",
     },
     {
-      title: "Blog",
-      href: "/blog",
+      title: "实时榜单",
+      href: "/charts",
     },
     {
-      title: "Documentation",
-      href: "/docs",
+      title: "极速切区",
+      href: "/store",
+    },
+    {
+      title: "免税地址",
+      href: "/address",
+    },
+    {
+      title: "教程中心",
+      href: "/tutorials",
+    },
+    {
+      title: "购买服务",
+      href: "/pricing", // Taxonomy 默认有个超美的 pricing 页面，咱们直接用！
     },
   ],
 }
