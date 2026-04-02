2705bd452772113b47fb47dcc744cf235cb38035
o614 <49578959+o614@users.noreply.github.com>
2026-04-02 11:35:50 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
+  name: "不要艾特我",
   description:
-    "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+    "全球数字游民的 App Store 观测站。洞察价格波动，掌握海外生态。",
+  url: "https://290935.xyz",
+  ogImage: "",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/taxonomy",
+    twitter: "",
+    github: "https://github.com/o614",
   },
 }
